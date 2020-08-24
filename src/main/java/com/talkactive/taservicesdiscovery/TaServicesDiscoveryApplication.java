b32bcaf5464d350338b830d3076f904f1238b453
bulzaj <ernestsado94@gmail.com>
2020-08-24 17:43:10 +0200

    first commit

---
+++ src/main/java/com/talkactive/taservicesdiscovery/TaServicesDiscoveryApplication.java
@@ -0,0 +1,13 @@
+package com.talkactive.taservicesdiscovery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaServicesDiscoveryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaServicesDiscoveryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/talkactive/taservicesdiscovery/TaServicesDiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.talkactive.taservicesdiscovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaServicesDiscoveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
